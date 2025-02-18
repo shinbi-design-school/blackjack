183a47da5daef4b09b33326736087477a635ff34
Satoshi Tanaka <satstnka@gmail.com>
2025-02-19 05:40:29 +0900

    初回のコミット

---
+++ src/com/design_shinbi/blackjack/BlackJack.java
@@ -0,0 +1,27 @@
+package com.design_shinbi.blackjack;
+
+import java.util.Scanner;
+
+import com.design_shinbi.blackjack.model.Attender;
+
+public class BlackJack {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		start(scanner);
+		
+		scanner.close();
+	}
+	
+	public static void start(Scanner scanner) {
+		
+		// 実装してみよう
+		
+	}
+	
+	public static void showResult(Attender player, Attender dealer) {
+		
+		// 実装してみよう
+		
+	}
+}
